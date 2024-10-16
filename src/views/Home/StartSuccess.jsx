67eaba8b336c1a9659c39e4d0fb1f31e0299a3a5
Devendra Mali <devendra.mali@digihost.in>
2024-10-16 18:07:55 +0530

    smooth slider changes

--- src/views/Home/StartSuccess.jsx
+++ src/views/Home/StartSuccess.jsx
@@ -1,26 +1,84 @@
-import React from 'react'
+import React from 'react';
 import { Container } from "react-bootstrap";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules"; // Only necessary modules
+import "swiper/css";
+import "swiper/css/autoplay";
+
 const StartSuccess = () => {
+
+    
+
     return (
-        <div className="section start-sucess">
+        <div className="section start-success">
             <Container fluid className="px-0">
-                <div className="marquee">
-                    <div className="headings">
-                        <h4>Start Your Success!</h4>
-                        <h4>Start Your Success!</h4>
-                        <h4>Start Your Success!</h4>
-                        <h4>Start Your Success!</h4>
+                <Swiper
+                    className='marquee'
+                    
+                    modules={[Autoplay]}
+                    spaceBetween={30} 
+                    slidesPerView={4.5} 
+                    loop={true} 
+                    autoplay={{
+                      delay: 0,
+                      disableOnInteraction: false,
+                    }}  
+                    speed={4000}
+              
+                >
+                    <SwiperSlide >
+                        <div className='headings'>
+                            <h4><a href="#!">Start Your Success!</a></h4>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className='headings'>
+                            <h4><a href="#!">Start Your Success!</a></h4>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className='headings'>
+                            <h4><a href="#!">Start Your Success!</a></h4>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className='headings'>
+                            <h4><a href="#!">Start Your Success!</a></h4>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className='headings'>
+                            <h4><a href="#!">Start Your Success!</a></h4>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className='headings'>
+                            <h4><a href="#!">Start Your Success!</a></h4>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className='headings'>
+                            <h4><a href="#!">Start Your Success!</a></h4>
+                        </div>
+                    </SwiperSlide>
+                    
+                   
+                   
+                </Swiper>
+                {/* <div className="marquee">
+                    <div className='headings'>
+                        <h4><a href="#!">Start Your Success!</a></h4>
+                        <h4><a href="#!">Start Your Success!</a></h4>
+                        <h4><a href="#!">Start Your Success!</a></h4>
+                        <h4><a href="#!">Start Your Success!</a></h4>
+                        <h4><a href="#!">Start Your Success!</a></h4>
+                        <h4><a href="#!">Start Your Success!</a></h4>
+                        <h4><a href="#!">Start Your Success!</a></h4>
                     </div>
-                    <div className="headings">
-                        <h4>Start Your Success!</h4>
-                        <h4>Start Your Success!</h4>
-                        <h4>Start Your Success!</h4>
-                        <h4>Start Your Success!</h4>
-                    </div>
-                </div>
+                </div> */}
             </Container>
         </div>
-    )
-}
+    );
+};
 
-export default StartSuccess
+export default StartSuccess;
