dd368d88429a5232a7db6e6b5b51c8332d2a7665
Sushant-Digihost <sushant.shirke@digihost.in>
2024-10-16 15:39:06 +0530

    unwanted files deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"; // Add useRef
 import Navbar from "./layouts/Navbar";
 import Home from "./views/Home";
 import Footer from "./layouts/Footer";
-import { Cta } from "./layouts/Cta";
 
 function App() {
   return (
