d78b8746eeca838dd5c6af6ed5312ff0c2fb0b3c
manisha-digihost <manisha.prajapati@digihost.in>
2024-10-25 10:40:19 +0530

    expert build

--- src/views/Home/ExpertyBuild.jsx
+++ src/views/Home/ExpertyBuild.jsx
@@ -8,6 +8,67 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const ExpertyBuild = () => {
+  //   const expertyBuild = useRef(null);
+
+  //   useEffect(() => {
+  //     const element = expertyBuild.current;
+
+  //     // Create the ScrollTrigger
+  //     const tl = gsap.timeline({
+  //       scrollTrigger: {
+  //         trigger: element,
+  //         start: "top 20%",
+  //         // markers: true,
+  //         scrub: 5,
+  //         end: "bottom 60%",
+  //         toggleActions: "play none none reverse",
+  //         // markers: true,
+  //         // pin: true,
+  //         scroller: "body",
+  //       },
+  //     });
+
+  //     tl.fromTo(
+  //       ".heading1",
+  //       { y: 0, opacity: 1 },
+  //       {
+  //         y: 0,
+  //         opacity: 1,
+  //         // duration: 2.5,
+  //         // ease: "power4.out",
+  //       },
+  //       "SS"
+  //     );
+
+  //     tl.fromTo(
+  //       ".heading2",
+  //       { y: 350, opacity: 0 },
+  //       {
+  //         y: -10,
+  //         opacity: 1,
+  //         duration: 2.5,
+  //         ease: "power4.out",
+  //       },
+  //       "SS"
+  //     );
+
+  //     tl.fromTo(
+  //       ".heading3",
+  //       { y: 700, opacity: 0 },
+  //       {
+  //         y: -10,
+  //         opacity: 1,
+  //         duration: 2.5,
+  //         ease: "power4.out",
+  //       },
+  //       "SS"
+  //     );
+
+  //     // Cleanup function to kill ScrollTrigger on component unmount
+  //     return () => {
+  //       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+  //     };
+  //   }, []);
   const expertyBuild = useRef(null);
 
   useEffect(() => {
@@ -18,47 +79,45 @@ const ExpertyBuild = () => {
       scrollTrigger: {
         trigger: element,
         start: "top 20%",
-        // markers: true,
-        scrub: 3,
-        end: "bottom 90%",
-        toggleActions: "play none none reverse",
-        // markers: true,
-        // pin: true,
+        end: "bottom 70%",
+        scrub: 5, // Allows smooth scrubbing on scroll
+        toggleActions: "play none none reverse", // Already set to reverse on scroll back
         scroller: "body",
       },
     });
 
+    // First heading remains static in position, as you defined
     tl.fromTo(
       ".heading1",
-      { y: 0, opacity: 0 },
+      { y: 0, opacity: 1 },
       {
         y: 0,
         opacity: 1,
-        duration: 1,
-        ease: "power4.out",
       },
       "SS"
     );
 
+    // Heading 2: Moves from y: 350 to y: -10 on scroll
     tl.fromTo(
       ".heading2",
       { y: 350, opacity: 0 },
       {
         y: -10,
         opacity: 1,
-        duration: 1,
+        duration: 2.5,
         ease: "power4.out",
       },
       "SS"
     );
 
+    // Heading 3: Moves from y: 700 to y: -10 on scroll
     tl.fromTo(
       ".heading3",
       { y: 700, opacity: 0 },
       {
         y: -10,
         opacity: 1,
-        duration: 1,
+        duration: 2.5,
         ease: "power4.out",
       },
       "SS"
@@ -69,6 +128,7 @@ const ExpertyBuild = () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
   }, []);
+
   return (
     <div className="section experty-build" ref={expertyBuild}>
       <Container fluid className="px-0">
