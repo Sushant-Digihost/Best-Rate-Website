3e00acce0f2702dc500c667b2660acb0b9b6f5c6
manisha-digihost <manisha.prajapati@digihost.in>
2025-02-07 18:06:06 +0530

    unlock benefits

--- src/views/CreatersClub.jsx
+++ src/views/CreatersClub.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import BannerCc from "./creaters-club/BannerCc";
 import WhoIsThis from "./creaters-club/WhoIsThis";
+import UnlockBenefits from "./creaters-club/UnlockBenefits";
 
 const CreatersClub = () => {
   return (
     <>
       <BannerCc />
       <WhoIsThis />
+      <UnlockBenefits />
     </>
   );
 };
---
+++ src/views/creaters-club/UnlockBenefits.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import circle from "./../../assets/images/creaters/unlock-bft.png";
+
+const UnlockBenefits = () => {
+  return (
+    <section className="section unlock-benifits">
+      <Container>
+        <div className="ul-card">
+          <h4>
+            Don’t let budget stop you from dreaming big. Join the Best Rate
+            Creators Club today for just ₹100 and take the first step toward
+            your online success.
+          </h4>
+          <Link to="#!" className="light-button mt-5">
+            Unlock Benefits
+          </Link>
+          <img src={circle} className="ul-bn" alt="Circle" />
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default UnlockBenefits;
